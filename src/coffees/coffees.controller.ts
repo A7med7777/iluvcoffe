367308380580e2e8069763af8ca7a285fff7db28
Ahmed Hussein <ahmedhessian56@gmail.com>
2025-07-26 01:57:57 +0300

    feat: integrate Swagger for API documentation; add decorators to DTOs and controller for enhanced API response handling

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -22,16 +22,24 @@ import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/paginati
 import { Public } from 'src/common/decorators/public.decorator';
 import { ParseIntPipe } from 'src/common/pipes/parse-int/parse-int.pipe';
 import { Protocol } from 'src/common/decorators/protocol.decorator';
+import { ApiForbiddenResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('coffees')
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
+  @ApiForbiddenResponse({
+    description: 'Forbidden',
+  })
   @UsePipes(ValidationPipe)
   // @SetMetadata('isPublic', true)
   @Public()
   @Get()
-  async findAll(@Protocol('https') protocol: string, @Query() paginationQuery: PaginationQueryDto) {
+  async findAll(
+    @Protocol('https') protocol: string,
+    @Query() paginationQuery: PaginationQueryDto,
+  ) {
     console.log(`Protocol: ${protocol}`);
     return this.coffeesService.findAll(paginationQuery);
   }
